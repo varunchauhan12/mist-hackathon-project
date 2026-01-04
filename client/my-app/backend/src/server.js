0e3196526be8493d281bb42ddde39fefa20ae818
KaranGupta_2006 <guptakaran.port@gmail.com>
2026-01-04 11:40:03 +0530

    backend setup

---
+++ client/my-app/backend/src/server.js
@@ -0,0 +1,47 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import http from "http";
+import { Server } from "socket.io";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/test",(req,res)=>{
+    res.send("Server is running..!!");
+})
+
+// socket.io setup
+const server=http.createServer(app);
+const io=new Server(server,{
+    cors:{
+        origin:"http://localhost:3000",
+        methods:["GET","POST"],
+        credentials:true
+    }
+});
+
+io.on("connection",(socket)=>{
+    console.log("New user connected:",socket.id);
+
+    socket.on("disconnect",()=>{
+        console.log("User disconnected:",socket.id);
+    })
+})
+
+server.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
